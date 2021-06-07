3075f95831903f09f48bc747874e830c49b8c7ec
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-06-07 22:12:59 +0100

    search works and updates with backspace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Icu } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
 import { ICharacter } from './shared/character.model';
 import { HpService } from './shared/hp.service';
@@ -10,9 +11,16 @@ import { HpService } from './shared/hp.service';
 export class AppComponent implements OnInit {
   hpCharacters: ICharacter[];
   searchTerm: string;
+  filtered;
 
-  
-  constructor(private services: HpService) {}
+  search(searchWord) {
+    let keyword = searchWord.target.value.toLowerCase();
+    this.filtered = this.hpCharacters.filter(character => {
+      return character.name.toLowerCase().includes(keyword) || character.house.toLowerCase().includes(keyword);
+  })
+  }
+
+  constructor(private services: HpService) { }
 
   ngOnInit() {
     this.fetchCharacters();
@@ -20,7 +28,7 @@ export class AppComponent implements OnInit {
   fetchCharacters() {
     this.services.fetchHpCharacters().subscribe(response => {
       this.hpCharacters = response;
-      console.log(this.hpCharacters);
+      this.filtered = this.hpCharacters;
     })
   }
 
